82de554eca3eac2c786104d7a127040150e0a574
dpolipnick <dick@onlinegrowthsystems.com>
2018-10-05 18:38:01 -0500

    Finishing Assignment

--- client.js
+++ client.js
@@ -11,7 +11,7 @@ class Employee{
     }//contructor
 }//end Employee class
 
-let employeesArray= [];
+let employeeArray= [];
 
 function readyNow() {
     console.log('jQuery is working.');
@@ -24,6 +24,44 @@ function getInfo() {
     let fNameIn=$('#fName').val();
     let lNameIn=$('#lName').val();
     let idNumberIn=$('#idNumber').val();
-    let jobTitleIn=$('#jobTitleIn').val();
+    let jobTitleIn=$('#jobTitle').val();
     let annualSalaryIn=$('#annualSalary').val();
+    let newEmployee = new Employee(fNameIn, lNameIn, idNumberIn, jobTitleIn, annualSalaryIn);
+    employeeArray.push(newEmployee);
+    console.log('New Employee Added');
+    updateChart();
+    clearForm();
+    updatePayroll();
 }//end getInfo function
+
+function updateChart() {
+    console.log('Inside updateDom function');
+    $('#chart').empty();
+    for (let employee of employeeArray) {
+        $('#chart').append('<li>'+ 'First Name: ' + employee.fName+ ' Last Name: ' + employee.lName+ ' ID#' + employee.idNumber+ ' Job Title: ' + employee.jobTitle+ ' Annual Salary $' + employee.annualSalary + '</li>');
+    }//end for of loop
+}//end updateDom function
+
+function clearForm() {
+    $('#fName').val('');
+    $('#lName').val('');
+    $('#idNumber').val('');
+    $('#jobTitle').val('');
+    $('#annualSalary').val('');
+}//end clearForm function
+
+function updatePayroll() {
+    let payroll = 0;
+    parseInt(payroll);
+    for (let payCheck of employeeArray) {
+        console.log(`Employee's Paycheck: $`, payCheck.annualSalary);
+        payroll = payroll + parseInt(payCheck.annualSalary);
+        console.log('New Payroll:', payroll);
+    }//end for of loop
+    $('#payroll').empty();
+    $('#payroll').append(`<h3>$` + payroll + `</h3>`);
+    if (payroll>20000) {
+        $('#payroll').empty();
+        $('#payroll').append(`<h3 class="red">$` + payroll + `</h3>`);
+    }
+}
